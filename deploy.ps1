934f8a1ba4f5619b5d8cbc70e6c11c9969951c3b
estela.polverini <01222093@sptech.com>
2026-04-11 19:02:51 -0300

    aa

---
+++ deploy.ps1
@@ -0,0 +1,85 @@
+Write-Host "Iniciando deploy do ETL na GCP..."
+
+# =========================
+# CONFIG
+# =========================
+$PROJECT_ID = "project-7dd9f12e-2611-4eb1-b18"
+$REGION = "us-central1"
+$REPO = "etl-repo"
+$IMAGE_NAME = "etl-data-wellness"
+$JOB_NAME = "etl-data-wellness"
+
+# =========================
+# VALIDACOES
+# =========================
+Write-Host "Verificando gcloud..."
+if (!(Get-Command gcloud -ErrorAction SilentlyContinue)) {
+    Write-Error "gcloud nao instalado."
+    exit 1
+}
+
+Write-Host "Verificando docker..."
+if (!(Get-Command docker -ErrorAction SilentlyContinue)) {
+    Write-Error "docker nao instalado."
+    exit 1
+}
+
+Write-Host "Verificando autenticacao..."
+$auth = gcloud auth list --format="value(account)"
+if (-not $auth) {
+    Write-Error "Voce nao esta autenticado no gcloud."
+    exit 1
+}
+
+Write-Host "Definindo projeto..."
+gcloud config set project $PROJECT_ID
+
+# =========================
+# CRIAR REPOSITORIO (se nao existir)
+# =========================
+Write-Host "Verificando Artifact Registry..."
+
+$repoExists = gcloud artifacts repositories list `
+    --filter="name:$REPO" `
+    --format="value(name)"
+
+if (-not $repoExists) {
+    Write-Host "Criando repositorio..."
+    gcloud artifacts repositories create $REPO `
+        --repository-format=docker `
+        --location=$REGION
+}
+
+# =========================
+# BUILD DA IMAGEM
+# =========================
+$IMAGE_URI = "$REGION-docker.pkg.dev/$PROJECT_ID/$REPO/$IMAGE_NAME:latest"
+
+Write-Host "Buildando imagem Docker..."
+docker build -t $IMAGE_URI .
+
+# =========================
+# PUSH
+# =========================
+Write-Host "Configurando docker auth..."
+gcloud auth configure-docker "$REGION-docker.pkg.dev" -q
+
+Write-Host "Enviando imagem..."
+docker push $IMAGE_URI
+
+# =========================
+# DEPLOY DO JOB
+# =========================
+Write-Host "Criando/Atualizando Cloud Run Job..."
+
+gcloud run jobs deploy $JOB_NAME `
+    --image $IMAGE_URI `
+    --region $REGION `
+    --set-env-vars "GCP_PROJECT_ID=$PROJECT_ID" `
+    --max-retries 1 `
+    --memory 512Mi `
+    --cpu 1 `
+    --quiet
+
+
+Write-Host "Deploy concluido com sucesso!"
