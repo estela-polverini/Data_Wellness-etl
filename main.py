5b42e25024c98bbe23a8e0d9c8bc0736c10243b3
estela.polverini <01222093@sptech.com>
2026-05-12 21:29:14 -0300

    Adicionando outro dataset

--- main.py
+++ main.py
@@ -11,22 +11,9 @@ PROJECT_ID = os.getenv("GCP_PROJECT_ID")
 DATASET = os.getenv("BQ_DATASET")
 
 
-def get_bq_client():
-    return bigquery.Client(project=PROJECT_ID)
 
+def extract_from_local(path : str) -> DataFrame:
 
-def load_to_bq(df: DataFrame, table_name: str):
-    client = get_bq_client()
-    table_id = f"{client.project}.{DATASET}.{table_name}"
-
-    job = client.load_table_from_dataframe(df, table_id)
-    job.result()
-
-    print(f"Dados enviados para {table_id}")
-
-
-def extract_from_local() -> DataFrame:
-    path = r'extract\materdei-data.csv'
     df = pd.read_csv(path)
 
     print(df.head())
@@ -36,6 +23,51 @@ def extract_from_local() -> DataFrame:
 
     return df
 
+def transform_data(df: DataFrame) -> DataFrame:
+    print("Iniciando transformação...")
+    #snake case
+    df.columns = (
+        df.columns
+        .str.lower()
+        .str.strip()
+        .str.replace(" ", "_")
+    )
+
+    # -1 para cd nulos
+    df["cd_unidade_atendimento"] = (
+        df["cd_unidade_atendimento"]
+        .fillna(-1)
+        .astype(int)
+    )
+
+    # ds_atendimento nulos
+    df["ds_unidade_atendimento"] = (
+        df["ds_unidade_atendimento"]
+        .fillna("Não Informado")
+    )
+
+    print("Datas inválidas:", 
+          df["dt_nascimento"].isna().sum())
+
+    #idade calculada
+    df["dt_nascimento"] = pd.to_datetime(
+        df["dt_nascimento"],
+        errors="coerce"
+    )
+    df["idade"] = calcular_idade(df["dt_nascimento"])
+    df["idade"] = (
+        df["idade"]
+        .fillna(-1)
+        .astype(int)
+    )
+
+    #alteração de tipagem
+    df["ano"] = df["ano"].astype(int)
+    df["cd_paciente"] = df["cd_paciente"].astype(int)
+
+    print("Transformação concluída!")
+
+    return df
 
 def calcular_idade(dt_nascimento: pd.Series) -> pd.Series:
     hoje = pd.Timestamp.today()
@@ -53,65 +85,34 @@ def calcular_idade(dt_nascimento: pd.Series) -> pd.Series:
 
     return idade
 
-
-def transform_data(df: DataFrame) -> DataFrame:
-    print("Iniciando transformação...")
-
-    #snake case
-    df.columns = (
-        df.columns
-        .str.lower()
-        .str.strip()
-        .str.replace(" ", "_")
-    )
-
-    #idade calculada
-    df["dt_nascimento"] = pd.to_datetime(
-        df["dt_nascimento"],
-        errors="coerce"
-    )
-    df["idade"] = calcular_idade(df["dt_nascimento"])
-    df["idade"] = (
-        df["idade"]
-        .fillna(-1)
-        .astype(int)
-    )
+def get_bq_client():
+    return bigquery.Client(project=PROJECT_ID)
 
 
-    # -1 para cd nulos
-    df["cd_unidade_atendimento"] = (
-        df["cd_unidade_atendimento"]
-        .fillna(-1)
-        .astype(int)
-    )
+def load_to_bq(df: DataFrame, table_name: str):
+    client = get_bq_client()
+    table_id = f"{client.project}.{DATASET}.{table_name}"
 
-    # ds_atendimento nulos
-    df["ds_unidade_atendimento"] = (
-        df["ds_unidade_atendimento"]
-        .fillna("Não Informado")
-    )
+    job = client.load_table_from_dataframe(df, table_id)
+    job.result()
 
-    #alteração de tipagem
-    df["ano"] = df["ano"].astype(int)
-    df["cd_paciente"] = df["cd_paciente"].astype(int)
-
-    print("Datas inválidas:", df["dt_nascimento"].isna().sum())
-
-    print("Transformação concluída!")
-
-    return df
-
+    print(f"Dados enviados para {table_id}")
 
 def main():
     print("Inicio do ETL...")
 
-    df_original = extract_from_local()
-    df_tratado = transform_data(df_original)
+    path_materdei = r'extract\materdei-data.csv'
+    path_novo = r'extract\dataset-no-shows.csv'
+
+    df_materdei = extract_from_local(path_materdei)
+    df_novo = extract_from_local(path_novo)
+
+    df_tratado = transform_data(df_materdei)
     print(df_tratado.head())
     df_tratado.info()
 
 
-    #load_to_bq(df_tratado, "materdei")
+    load_to_bq(df_tratado, "materdei")
 
     print("ETL finalizado!")
 
